e4620d5d87cd03d63fb2c66245bd44c5a90d5589
pawelsiemieniuk <pawel.siemieniuk11@gmail.com>
2025-01-13 19:43:00 +0100

    Przygotowanie terminala, narysowanie interfejsu i rozpoczęcie pętli(na ten moment pusta). Przeniesienie interfejsu ViewController do paczki view.

--- src/game/Game.java
+++ src/game/Game.java
@@ -1,10 +1,20 @@
 package game;
 
+import java.util.stream.Stream;
+
+import com.googlecode.lanterna.TerminalSize;
+
+import maze.Field;
 import maze.Maze;
+import view.IViewController;
+import view.View;
 
 public class Game implements EventListener, GameController {
-	private ViewController viewController;
+	private IViewController viewController;
 	private Maze maze;
+	
+	private int gameWidth  = 32,
+				gameHeight = 32;
 	
 	private int score;
 	private int level;
@@ -14,11 +24,42 @@ public class Game implements EventListener, GameController {
 	
 	
 	public Game() {
+		viewController = View.getView();
+		maze = new Maze();
+		maze.GenerateMaze(gameWidth, gameHeight);
+	}
+	
+	public void StartGame() {
+		viewController.OpenWindow(new TerminalSize(46, 40));
+		viewController.SetupWindow();
+		try {
+			viewController.SetupGameView(gameWidth, gameHeight);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		DrawGame();
 		
+		Loop();
+	}
+	
+	private void DrawGame() {
+		Field[] fieldsToDraw = (Field[]) Stream.of(maze.getFields()).flatMap(Stream::of).toArray(Field[]::new);
+		try {
+			viewController.DrawGame(fieldsToDraw);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
 	private void Loop() {
-		
+		while(true) {
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
 
 	@Override
--- src/game/GameController.java
+++ src/game/GameController.java
@@ -1,5 +1,5 @@
 package game;
 
 public interface GameController {
-	
+	public void StartGame();
 }
